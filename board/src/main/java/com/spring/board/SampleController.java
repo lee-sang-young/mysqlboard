36bad6156e40c258b1a2a4ec05e14a8a9b3a79e5
lee-sang-young <62328421+lee-sang-young@users.noreply.github.com>
2020-07-19 11:33:12 +0900

    update insert delete

--- board/src/main/java/com/spring/board/SampleController.java
+++ board/src/main/java/com/spring/board/SampleController.java
@@ -8,15 +8,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping(value = "/sample")
 public class SampleController {
-	
-	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping(value = "/getSample")
 	public void getSample(HttpServletRequest request, HttpServletResponse reponse) throws Exception {
-		logger.debug("====================getSample====================");
+		System.out.println("getSample");
 	}
 }
